8ed69fb1475e2238e0bfac5e00b7a506befd3b52
minhtrifit <minhtri.fit@gmail.com>
2025-10-25 10:07:28 +0700

    fix: carrer-section mobile view

--- src/pages/home/components/carrer-section/CarrerSection.tsx
+++ src/pages/home/components/carrer-section/CarrerSection.tsx
@@ -3,12 +3,14 @@ import { get } from 'lodash';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import { Tag } from 'antd';
 import { useTranslation } from 'react-i18next';
+import { useIsMobile } from '@/hooks/useIsMobile';
 import { TimelineType } from '@/types';
 import { FaPen } from 'react-icons/fa';
 import { MdOutlineWork } from 'react-icons/md';
 
 const CarrerSection = () => {
   const { t, i18n } = useTranslation();
+  const isMobile = useIsMobile();
 
   const TIME_LINE: TimelineType[] = [
     {
@@ -49,7 +51,7 @@ const CarrerSection = () => {
             return (
               <VerticalTimelineElement
                 key={uuidv4()}
-                date={get(tl, 'date', '')}
+                date={isMobile ? '' : get(tl, 'date', '')}
                 contentStyle={{
                   background: '#0f172a',
                   color: '#fff',
@@ -60,9 +62,17 @@ const CarrerSection = () => {
                 iconStyle={{ background: '#14b3ea', color: '#fff' }}
                 icon={get(tl, 'icon', <></>)}
               >
-                <img src={get(tl, 'logo', '')} className='w-[50px]' />
-                <div>
-                  <h3 className='text-primary my-0'>{get(tl, `title_${i18n.language}`, '')}</h3>
+                {!isMobile && <img src={get(tl, 'logo', '')} className='w-[50px]' />}
+
+                <div className='flex flex-col gap-1'>
+                  <div className='flex items-start gap-[20px]'>
+                    {isMobile && <img src={get(tl, 'logo', '')} className='w-[50px]' />}
+                    <h3 className='text-primary my-0'>{get(tl, `title_${i18n.language}`, '')}</h3>
+                  </div>
+
+                  {isMobile && (
+                    <span className='my-3 text-[0.8rem] text-zinc-400'>{get(tl, 'date', '')}</span>
+                  )}
 
                   <div className='mt-3 flex flex-wrap items-center gap-2'>
                     {tl.skils.map((skill: string) => {
