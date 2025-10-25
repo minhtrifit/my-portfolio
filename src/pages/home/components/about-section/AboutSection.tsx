67de25072be958a61b0dbb7fa095b8f09c0c643c
minhtrifit <minhtri.fit@gmail.com>
2025-10-25 19:34:16 +0700

    feat: update about-section

--- src/pages/home/components/about-section/AboutSection.tsx
+++ src/pages/home/components/about-section/AboutSection.tsx
@@ -1,11 +1,13 @@
 import { v4 as uuidv4 } from 'uuid';
 import { get } from 'lodash';
 import Marquee from 'react-fast-marquee';
-import { Button } from 'antd';
+import { Button, Card } from 'antd';
 import { useInView } from 'react-intersection-observer';
 import { useTranslation } from 'react-i18next';
 import { FaPhone } from 'react-icons/fa';
 import { TechStackType } from '@/types';
+
+import styles from './styles.module.scss';
 
 const AboutSection = () => {
   const TECH_STACK: TechStackType[] = [
@@ -13,9 +15,11 @@ const AboutSection = () => {
     { name: 'css', logo: '/assets/icons/technicals/css.png' },
     { name: 'javascript', logo: '/assets/icons/technicals/javascript.png' },
     { name: 'typescript', logo: '/assets/icons/technicals/typescript.png' },
+    { name: 'git', logo: '/assets/icons/technicals/git.png' },
     { name: 'reactjs', logo: '/assets/icons/technicals/reactjs.png' },
     { name: 'nextjs', logo: '/assets/icons/technicals/nextjs.png' },
     { name: 'tailwindcss', logo: '/assets/icons/technicals/tailwindcss.png' },
+    { name: 'bootstrap', logo: '/assets/icons/technicals/bootstrap.png' },
     { name: 'nodejs', logo: '/assets/icons/technicals/nodejs.png' },
     { name: 'nestjs', logo: '/assets/icons/technicals/nestjs.png' },
     { name: 'postgresql', logo: '/assets/icons/technicals/postgresql.png' },
@@ -33,6 +37,10 @@ const AboutSection = () => {
     triggerOnce: true,
     threshold: 0.5,
   });
+
+  const { ref: skillRef1, inView: skillInView1 } = useInView({ triggerOnce: true, threshold: 0.5 });
+  const { ref: skillRef2, inView: skillInView2 } = useInView({ triggerOnce: true, threshold: 0.5 });
+  const { ref: skillRef3, inView: skillInView3 } = useInView({ triggerOnce: true, threshold: 0.5 });
 
   return (
     <section id='about-section' className='w-full flex bg-secondary-gray overflow-x-hidden'>
@@ -65,8 +73,93 @@ const AboutSection = () => {
           </div>
         </div>
 
+        {/* Skills Block */}
+        <div className='mt-10 col-span-full'>
+          <h2 className='text-[2rem] text-primary text-center'>{t('skills')}</h2>
+
+          <div className='mt-5 mb-10 flex items-center justify-center'>
+            <span className='text-center'>{t('skills-description')}</span>
+          </div>
+
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            <Card
+              ref={skillRef1}
+              className={`opacity-0 transition-opacity duration-700 delay-[0ms]
+                          ${styles.skill__card}
+                          ${skillInView1 ? 'opacity-100 animate__animated animate__fadeInLeft' : ''}
+                        `}
+            >
+              <img
+                className='w-full h-[300px] rounded-t-md object-cover'
+                src='/assets/images/working.png'
+                alt='about-working-img'
+              />
+
+              <div className='flex flex-col p-3'>
+                <h3 className='my-0 h-[56px] line-clamp-2 text-center text-primary font-semibold text-lg'>
+                  {t('skills-list.title-1')}
+                </h3>
+                <p className='text-center text-sm text-gray-600 h-[60px] line-clamp-3'>
+                  {t('skills-list.description-1')}
+                </p>
+              </div>
+            </Card>
+
+            <Card
+              ref={skillRef2}
+              className={`opacity-0 transition-opacity duration-700 delay-[300ms]
+                          ${styles.skill__card}
+                          ${skillInView2 ? 'opacity-100 animate__animated animate__fadeInLeft' : ''}
+                        `}
+            >
+              <img
+                className='w-full h-[300px] rounded-t-md object-cover'
+                src='/assets/images/coding.png'
+                alt='about-coding-img'
+              />
+
+              <div className='flex flex-col p-3'>
+                <h3 className='my-0 h-[56px] line-clamp-2 text-center text-primary font-semibold text-lg'>
+                  {t('skills-list.title-2')}
+                </h3>
+                <p className='text-center text-sm text-gray-600 h-[60px] line-clamp-3'>
+                  {t('skills-list.description-2')}
+                </p>
+              </div>
+            </Card>
+
+            <Card
+              ref={skillRef3}
+              className={`opacity-0 transition-opacity duration-700 delay-[600ms]
+                          ${styles.skill__card}
+                          ${skillInView3 ? 'opacity-100 animate__animated animate__fadeInLeft' : ''}
+                        `}
+            >
+              <img
+                className='w-full h-[300px] rounded-t-md object-cover'
+                src='/assets/images/study.png'
+                alt='about-eat-study-img'
+              />
+
+              <div className='flex flex-col p-3'>
+                <h3 className='my-0 h-[56px] line-clamp-2 text-center text-primary font-semibold text-lg'>
+                  {t('skills-list.title-3')}
+                </h3>
+                <p className='text-center text-sm text-gray-600 h-[60px] line-clamp-3'>
+                  {t('skills-list.description-3')}
+                </p>
+              </div>
+            </Card>
+          </div>
+        </div>
+
+        {/* Tech Stack Block */}
         <div className='mt-10 col-span-full'>
           <h2 className='text-[2rem] text-primary text-center'>{t('tech-stack')}</h2>
+
+          <div className='mt-5 mb-10 flex items-center justify-center'>
+            <span className='text-center'>{t('tech-description')}</span>
+          </div>
 
           <Marquee autoFill>
             {TECH_STACK.map((tech: { name: string; logo: string }) => {
--- src/pages/home/components/hero-section/HeroSection.tsx
+++ src/pages/home/components/hero-section/HeroSection.tsx
@@ -1,10 +1,15 @@
 import Typewriter from 'typewriter-effect';
+import { Button } from 'antd';
 import { useInView } from 'react-intersection-observer';
+import { useScrollToId } from '@/hooks/useScrollToId';
 import { useTranslation } from 'react-i18next';
+import { FaSearch } from 'react-icons/fa';
+import { GrDocumentDownload } from 'react-icons/gr';
 
 import styles from './styles.module.scss';
 
 const HeroSection = () => {
+  const scrollToId = useScrollToId(70);
   const { t } = useTranslation();
 
   const { ref, inView } = useInView({
@@ -17,72 +22,95 @@ const HeroSection = () => {
     threshold: 0.2,
   });
 
+  const handleLearnMore = () => {
+    scrollToId('about-section');
+  };
+
+  const handleDownloadFile = () => {
+    const link = document.createElement('a');
+    link.href = '/assets/documents/file-sample.pdf';
+    link.download = 'file-sample.pdf';
+    link.click();
+  };
+
   return (
     <section id='home-section' className='w-full flex overflow-x-hidden'>
       <div
         className={`mx-auto w-full max-w-[1024px] min-h-[calc(100vh-70px)] px-[16px]
-                    flex flex-wrap items-center justify-center sm:justify-between gap-[50px]`}
+                      flex flex-col justify-center gap-[30px]`}
       >
-        <div
-          ref={ref}
-          className={`opacity-0 transition-opacity duration-700
+        <div className='flex flex-wrap items-center justify-center sm:justify-between gap-[50px]'>
+          <div
+            ref={ref}
+            className={`opacity-0 transition-opacity duration-700
                         flex flex-col items-center sm:items-start
                         ${inView ? 'opacity-100 animate__animated animate__zoomIn' : ''}
                     `}
-        >
-          <h1 className='text-primary text-[4rem]'>{t('hello')},</h1>
+          >
+            <h1 className='text-primary text-[3rem] sm:text-[4rem]'>{t('hello')},</h1>
 
-          <Typewriter
-            options={{
-              strings: [t('i-am-tri'), t('software-engineer')],
-              autoStart: true,
-              loop: true,
-              delay: 60,
-              deleteSpeed: 40,
-              wrapperClassName: styles.text, // SCSS module class
-              cursorClassName: styles.cursor, // SCSS module class
-            }}
-          />
+            <Typewriter
+              options={{
+                strings: [t('i-am-tri'), t('software-engineer')],
+                autoStart: true,
+                loop: true,
+                delay: 60,
+                deleteSpeed: 40,
+                wrapperClassName: styles.text, // SCSS module class
+                cursorClassName: styles.cursor, // SCSS module class
+              }}
+            />
 
-          <div className='mt-8 flex flex-wrap items-center gap-2'>
-            <a href='https://facebook.com/minhtrifit' target='_blank' rel='noopener noreferrer'>
-              <img
-                className='w-12 h-auto hover:translate-y-1'
-                src='/assets/icons/social-media/facebook.png'
-                alt='facebook-icon'
-              />
-            </a>
+            <div className='mt-8 flex flex-wrap items-center gap-2'>
+              <a href='https://facebook.com/minhtrifit' target='_blank' rel='noopener noreferrer'>
+                <img
+                  className='w-12 h-auto hover:translate-y-1'
+                  src='/assets/icons/social-media/facebook.png'
+                  alt='facebook-icon'
+                />
+              </a>
 
-            <a href='https://youtube.com/@minhtrifit' target='_blank' rel='noopener noreferrer'>
-              <img
-                className='w-12 h-auto hover:translate-y-1'
-                src='/assets/icons/social-media/youtube.png'
-                alt='facebook-icon'
-              />
-            </a>
+              <a href='https://youtube.com/@minhtrifit' target='_blank' rel='noopener noreferrer'>
+                <img
+                  className='w-12 h-auto hover:translate-y-1'
+                  src='/assets/icons/social-media/youtube.png'
+                  alt='facebook-icon'
+                />
+              </a>
 
-            <a
-              href='https://linkedin.com/in/lê-minh-trí-89ab94215'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <img
-                className='w-12 h-auto hover:translate-y-1'
-                src='/assets/icons/social-media/linkedin.png'
-                alt='facebook-icon'
-              />
-            </a>
+              <a
+                href='https://linkedin.com/in/lê-minh-trí-89ab94215'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <img
+                  className='w-12 h-auto hover:translate-y-1'
+                  src='/assets/icons/social-media/linkedin.png'
+                  alt='facebook-icon'
+                />
+              </a>
+            </div>
           </div>
-        </div>
 
-        <img
-          ref={imgRef}
-          className={`opacity-0 transition-opacity duration-700 w-[350px]
+          <img
+            ref={imgRef}
+            className={`opacity-0 transition-opacity duration-700 w-[200px] sm:w-[350px]
                         ${imgInView ? 'opacity-100 animate__animated animate__zoomIn' : ''}
                     `}
-          src='/assets/images/sword-pose-2.png'
-          alt='hero-img'
-        />
+            src='/assets/images/sword-pose-2.png'
+            alt='hero-img'
+          />
+        </div>
+
+        <div className='mb-5 w-full flex flex-wrap items-center justify-center gap-3'>
+          <Button icon={<FaSearch />} onClick={handleLearnMore}>
+            {t('learn-more')}
+          </Button>
+
+          <Button type='primary' icon={<GrDocumentDownload />} onClick={handleDownloadFile}>
+            {t('download-resume')}
+          </Button>
+        </div>
       </div>
     </section>
   );
